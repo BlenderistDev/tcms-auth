0e7fd8c4a1962b57d1e4abc3b9dcbb4844f1cea7
BlenderistDev <demonx.blenderist@gmail.com>
2022-04-29 20:52:16 +0300

    add AuthGrpcService Login error tests

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -159,3 +159,149 @@ func TestAuthGrpcService_Login(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, token, res.Token)
 }
+
+func TestAuthGrpcService_Login_userRepoError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	const username = "name"
+	const password = "password"
+
+	resErr := errors.New("some error")
+
+	userRepo := mock_repository.NewMockUserRepository(ctrl)
+	userRepo.EXPECT().GetUser(gomock.Eq(username)).Return(nil, resErr)
+
+	sessionRepo := mock_repository.NewMockSessionRepository(ctrl)
+
+	passwordGenerator := mock_password.NewMockGenerator(ctrl)
+
+	service := AuthGrpcService{
+		UserRepo:          userRepo,
+		SessionRepo:       sessionRepo,
+		PasswordGenerator: passwordGenerator,
+	}
+
+	authData := &auth.AuthData{
+		Username: username,
+		Password: password,
+	}
+
+	res, err := service.Login(context.Background(), authData)
+	assert.Nil(t, res)
+	assert.Equal(t, resErr, err)
+}
+
+func TestAuthGrpcService_Login_noUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	const username = "name"
+	const password = "password"
+
+	userRepo := mock_repository.NewMockUserRepository(ctrl)
+	userRepo.EXPECT().GetUser(gomock.Eq(username)).Return(nil, nil)
+
+	sessionRepo := mock_repository.NewMockSessionRepository(ctrl)
+
+	passwordGenerator := mock_password.NewMockGenerator(ctrl)
+
+	service := AuthGrpcService{
+		UserRepo:          userRepo,
+		SessionRepo:       sessionRepo,
+		PasswordGenerator: passwordGenerator,
+	}
+
+	authData := &auth.AuthData{
+		Username: username,
+		Password: password,
+	}
+
+	res, err := service.Login(context.Background(), authData)
+	assert.Nil(t, res)
+	assert.Equal(t, ErrNoUser, err)
+}
+
+func TestAuthGrpcService_Login_wrongPassword(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	const userId = 1
+	const username = "name"
+	const password = "password"
+	const passwordHash = "passwordHash"
+
+	resErr := errors.New("some error")
+
+	user := &model.User{
+		Id:       userId,
+		Username: username,
+		Password: passwordHash,
+	}
+
+	userRepo := mock_repository.NewMockUserRepository(ctrl)
+	userRepo.EXPECT().GetUser(gomock.Eq(username)).Return(user, nil)
+
+	sessionRepo := mock_repository.NewMockSessionRepository(ctrl)
+
+	passwordGenerator := mock_password.NewMockGenerator(ctrl)
+	passwordGenerator.EXPECT().Compare(gomock.Eq(passwordHash), gomock.Eq(password)).Return(resErr)
+
+	service := AuthGrpcService{
+		UserRepo:          userRepo,
+		SessionRepo:       sessionRepo,
+		PasswordGenerator: passwordGenerator,
+	}
+
+	authData := &auth.AuthData{
+		Username: username,
+		Password: password,
+	}
+
+	res, err := service.Login(context.Background(), authData)
+	assert.Nil(t, res)
+	assert.Equal(t, ErrWrongPassword, err)
+}
+
+func TestAuthGrpcService_Login_sessionRepoError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	const userId = 1
+	const username = "name"
+	const password = "password"
+	const passwordHash = "passwordHash"
+	const token = "token"
+
+	resErr := errors.New("some error")
+
+	user := &model.User{
+		Id:       userId,
+		Username: username,
+		Password: passwordHash,
+	}
+
+	userRepo := mock_repository.NewMockUserRepository(ctrl)
+	userRepo.EXPECT().GetUser(gomock.Eq(username)).Return(user, nil)
+
+	sessionRepo := mock_repository.NewMockSessionRepository(ctrl)
+	sessionRepo.EXPECT().Create(gomock.Eq(userId)).Return(token, resErr)
+
+	passwordGenerator := mock_password.NewMockGenerator(ctrl)
+	passwordGenerator.EXPECT().Compare(gomock.Eq(passwordHash), gomock.Eq(password)).Return(nil)
+
+	service := AuthGrpcService{
+		UserRepo:          userRepo,
+		SessionRepo:       sessionRepo,
+		PasswordGenerator: passwordGenerator,
+	}
+
+	authData := &auth.AuthData{
+		Username: username,
+		Password: password,
+	}
+
+	res, err := service.Login(context.Background(), authData)
+	assert.Nil(t, res)
+	assert.Equal(t, err, resErr)
+}
